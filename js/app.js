0e57bae32f48c244f4f3d1e5a2a70585a565b5a8
Nathoriz <nathortiz_41@hotmail.com>
2018-01-15 17:09:58 -0500

    Haciendo cambios en funcionalidad

--- js/app.js
+++ js/app.js
@@ -4,44 +4,44 @@ $(document).ready(function() {
   var $tweetButton =  $('#tweet');
   var $count = $('#words');
   var $maxCharacters  =  140;
-  
+  var $containerComents = $("#container-coments");
+
   $tweetButton.click(function() {
     if($textarea.val().length) {
-      var comment = $textarea.val();
-      var timepicker = $('.timepicker').wickedpicker();
-      var time = timepickers.wickedpicker('time'); 
-      var $newComment = $("<div class='col-lg-12 light-blue lighten-4 coments padding-twitter'><div class='row'><div class='col-lg-2'><img class='col-lg-12 rounded-circle' src='assets/images/white-fox.jpg' alt=''></div><div class='col-lg-10 coment'><div class='col-lg-11 rounded white'><p>" + comment + "</p></div></div></div><div class='row'><div class='col-lg-7'></div><div class='col-lg-auto'><span class='btn btn-outline-primary btn-rounded white'>Publicado: <span>" + time + "</span> </span></div></div></div>");
-      $newComment.insertAfter("#spaceToComent");
-    
-    }else {
-      $tweetButton.attr('disabled', 'true');
+      var $comment = $textarea.val();
+      var $newComment = $("<div class='col-lg-12 light-blue lighten-4 coments padding-twitter'><div class='row'><div class='col-lg-2'><img class='col-lg-12 rounded-circle' src='assets/images/white-fox.jpg'></div><div class='col-lg-10 coment'><div class='col-lg-auto rounded white padding-twitter myComent'>"+$comment+"</div></div></div><div class='row'><div class='col-lg-7'></div><div class='col-lg-auto'><span class='btn btn-outline-primary btn-rounded white mrg-btn'>Publicado:<span id='#setTime'></span></span></div></div></div>");
+      $containerComents.prepend($newComment);
     }
   });
 
   $textarea.keyup(function() {
-    var characters = $textarea.val().length;
-    var total = $maxCharacters - characters;
-    $count.text(total);
-    if (140 >= total && total > 20 ) {
+    var $characters = $textarea.val().length;
+    var $total = $maxCharacters - $characters;
+    $count.text($total);
+    if(140 >= $total && $total > 20 ) {
       $count.removeClass('btn-outline-primary');
       $count.removeClass('btn-outline-secondary');
       $count.removeClass('btn-outline-warning');
       $count.removeClass('btn-outline-danger');
       $count.addClass('btn-outline-primary');
-      
-    }else if (total <= 20 && total > 10){
+      $tweetButton.removeAttr('disabled');
+
+    }else if($total <= 20 && $total > 10){
       $count.removeClass('btn-outline-primary');
       $count.removeClass('btn-outline-warning');
       $count.removeClass('btn-outline-danger');
       $count.addClass('btn-outline-secondary');
+      $tweetButton.removeAttr('disabled');
       
-    }else if(total <= 10 && total > 0) {
+    }else if($total <= 10 && $total > 0) {
       $count.removeClass('btn-outline-primary');
       $count.removeClass('btn-outline-secondary');
       $count.removeClass('btn-outline-danger');
       $count.addClass('btn-outline-warning');
+
+      $tweetButton.removeAttr('disabled');
       
-    }else if(total <= 0) {
+    }else if($total <= 0) {
       $count.removeClass('btn-outline-primary');
       $count.removeClass('btn-outline-secondary');
       $count.removeClass('btn-outline-warning');
@@ -49,5 +49,5 @@ $(document).ready(function() {
       $tweetButton.attr('disabled', 'true');
     }  
   });
-  
+
 });
