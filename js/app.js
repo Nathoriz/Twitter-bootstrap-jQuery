08d1620da29a17b2c464082bbcafd15ff5c33e79
Nathoriz <nathortiz_41@hotmail.com>
2018-01-24 01:02:10 -0500

    Twitter responsive y arregalando los comentarios

--- js/app.js
+++ js/app.js
@@ -1,15 +1,37 @@
 $(document).ready(function() {
   var $spaceToComent = $('#spaceToComent');
-  var $textarea =  $('#whatYouThink');
-  var $tweetButton =  $('#tweet');
+  var $textarea = $('#whatYouThink');
+  var $tweetButton = $('#tweet');
   var $count = $('#words');
-  var $maxCharacters  =  140;
-  var $containerComents = $("#container-coments");
+  var $maxCharacters = 140;
+  var $containerComents = $('#container-coments');
 
   $tweetButton.click(function() {
-    if($textarea.val().length) {
+    if ($textarea.val().length) {
       var $comment = $textarea.val();
-      var $newComment = $("<div class='col-lg-12 light-blue lighten-4 coments padding-twitter'><div class='row'><div class='col-lg-2'><img class='col-lg-12 rounded-circle' src='assets/images/white-fox.jpg'></div><div class='col-lg-10 coment'><div class='col-lg-auto rounded white padding-twitter myComent'>"+$comment+"</div></div></div><div class='row'><div class='col-lg-7'></div><div class='col-lg-auto'><span class='btn btn-outline-primary btn-rounded white mrg-btn'>Publicado:<span id='#setTime'></span></span></div></div></div>");
+      var $newComment = $(
+        "<div class='col-lg-12 light-blue lighten-4 p-3'>" +
+          "<div class='row'>" + 
+            "<div class='col-xl-2 col-lg-3 col-md-3 col-sm-2 col-4'>" +
+              "<img class='col-12 rounded-circle' src='assets/images/white-fox.jpg'>" +
+            "</div>" +
+            "<div class='col-xl-10 col-lg-9 col-md-9 col-sm-10 col-8'>"+ 
+              "<div class='col-lg-auto rounded white padding-twitter myComent'>" 
+                + $comment +
+              "</div>" +
+            "</div>" +
+          "</div>" +
+          "<div class='row justify-content-end'>" +
+            "<div class='col-auto'>" +
+              "<span class='btn btn-outline-primary btn-rounded white mrg-btn'>Publicado:" +
+                "<span id='#setTime'>" +
+                  ' Hora' +
+                "</span>" +
+              "</span>" +
+            "</div>" +
+          "</div>" +
+        "</div>"
+      );
       $containerComents.prepend($newComment);
     }
   });
@@ -18,36 +40,32 @@ $(document).ready(function() {
     var $characters = $textarea.val().length;
     var $total = $maxCharacters - $characters;
     $count.text($total);
-    if(140 >= $total && $total > 20 ) {
+    if (140 >= $total && $total > 20) {
       $count.removeClass('btn-outline-primary');
       $count.removeClass('btn-outline-secondary');
       $count.removeClass('btn-outline-warning');
       $count.removeClass('btn-outline-danger');
       $count.addClass('btn-outline-primary');
       $tweetButton.removeAttr('disabled');
-
-    }else if($total <= 20 && $total > 10){
+    } else if ($total <= 20 && $total > 10) {
       $count.removeClass('btn-outline-primary');
       $count.removeClass('btn-outline-warning');
       $count.removeClass('btn-outline-danger');
       $count.addClass('btn-outline-secondary');
       $tweetButton.removeAttr('disabled');
-      
-    }else if($total <= 10 && $total > 0) {
+    } else if ($total <= 10 && $total > 0) {
       $count.removeClass('btn-outline-primary');
       $count.removeClass('btn-outline-secondary');
       $count.removeClass('btn-outline-danger');
       $count.addClass('btn-outline-warning');
 
       $tweetButton.removeAttr('disabled');
-      
-    }else if($total <= 0) {
+    } else if ($total <= 0) {
       $count.removeClass('btn-outline-primary');
       $count.removeClass('btn-outline-secondary');
       $count.removeClass('btn-outline-warning');
       $count.addClass('btn-outline-danger');
       $tweetButton.attr('disabled', 'true');
-    }  
+    }
   });
-
 });
